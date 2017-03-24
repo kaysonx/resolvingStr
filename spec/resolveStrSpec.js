2c4fee3e9780893efae28200e4a12e036f5402df
utlight <joker_e@163.com>
2017-03-24 19:21:35 +0800

    finish the case:give the different word string

---
+++ spec/resolveStrSpec.js
@@ -0,0 +1,17 @@
+
+
+describe('Resolve the String', () => {
+    
+    it('should get word&count when given the different word string', () => {
+        let testStr = `git remote add origin`
+        let expectResult = [
+            {word:'git',count:1},
+            {word:'remote',count:1},
+            {word:'add',count:1},
+            {word:'origin',count:1}
+        ]
+        expect(resolveStr(testStr)).toBe(expectResult)
+    });
+        
+});
+    
