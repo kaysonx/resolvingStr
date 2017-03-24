36b3859ce6a007e9e4c370dd4d87d69c2020f89e
utlight <joker_e@163.com>
2017-03-24 19:28:58 +0800

    realize the first feature

--- spec/resolveStrSpec.js
+++ spec/resolveStrSpec.js
@@ -1,17 +1,16 @@
 
 
 describe('Resolve the String', () => {
-    
+    let resolveStr = require('../lib/resolveStr')
     it('should get word&count when given the different word string', () => {
         let testStr = `git remote add origin`
         let expectResult = [
-            {word:'git',count:1},
-            {word:'remote',count:1},
-            {word:'add',count:1},
-            {word:'origin',count:1}
+            { word: 'git', count: 1 },
+            { word: 'remote', count: 1 },
+            { word: 'add', count: 1 },
+            { word: 'origin', count: 1 }
         ]
-        expect(resolveStr(testStr)).toBe(expectResult)
+        expect(resolveStr(testStr)).toEqual(expectResult)
     });
-        
+
 });
-    
