f7ed0a7d37ffbd994df517074b5ef2abc56a8951
utlight <joker_e@163.com>
2017-03-27 10:28:43 +0800

    refactoring the test case

--- spec/resolveStrSpec.js
+++ spec/resolveStrSpec.js
@@ -3,34 +3,26 @@
 describe('Resolve the String', () => {
     let resolveStr = require('../lib/resolveStr')
     it('should get word&count when given the word string is all different', () => {
-        let testStr = `git remote add origin`
+        let testStr = `git`
         let expectResult = [
-            { word: 'git', count: 1 },
-            { word: 'remote', count: 1 },
-            { word: 'add', count: 1 },
-            { word: 'origin', count: 1 }
+            { word: 'git', count: 1 }
         ]
         expect(resolveStr(testStr)).toEqual(expectResult)
     });
 
     it('should get word&count when given the word string contain same word', () => {
-        let testStr = `git remote add origin git remote add git remote git`
+        let testStr = `git git git`
         let expectResult = [
-            { word: 'git', count: 4 },
-            { word: 'remote', count: 3 },
-            { word: 'add', count: 2 },
-            { word: 'origin', count: 1 }
+            { word: 'git', count: 3 }
         ]
         expect(resolveStr(testStr)).toEqual(expectResult)
     });
 
     it('should get word&count and sort desc when given the word string', () => {
-        let testStr = `git remote add origin remote add add remote git remote remote remote remote`
+        let testStr = `add add add remote remote remote remote`
         let expectResult = [
-            { word: 'remote', count: 7 },
-            { word: 'add', count: 3 },
-            { word: 'git', count: 2 },
-            { word: 'origin', count: 1 }
+            { word: 'remote', count: 4 },
+            { word: 'add', count: 3 }
         ]
         expect(resolveStr(testStr)).toEqual(expectResult)
     });
